d932638b722ce75eefa5e754fe8d6b9f8fa43d6b
Forbrig <vitorforbrig@gmail.com>
2018-04-23 17:48:41 -0300

    function to store qtables, plus score 200 qtable

--- source/cartpole.py
+++ source/cartpole.py
@@ -1,6 +1,7 @@
 import numpy as np
 import gym
 import random
+import cPickle as pickle
 import time
 
 env = gym.make('CartPole-v0')
@@ -9,9 +10,11 @@ env.reset()
 Q = np.zeros([40, 400, 40, 2]) # q-table, where we store the states, actions and rewars
 learning_rate = 0.8
 y = 0.95
-num_train_episodes = 10000 # with how many episodes our agent will train
-num_test_episodes = 100 # how mani episodes our agent will be tested
+num_train_episodes = 50000 # with how many episodes our agent will train
+num_test_episodes = 100 # how many episodes our agent will be tested
 render = False # render our test (will slow the respose a lot)
+slow_render = True # slows 0.2 secs each step the render of the test (don't work if render = False)
+use_saved_qtable = False
 
 # Q = [40][400][40][2]
 # [40] = velocities from -2 to 2 (goes from -inf to inf)
@@ -42,6 +45,24 @@ def discrete_indexes(state):
     state = (car_velocity, pole_angle, pole_velocity)
     return state
 
+# save qtable and his average score (over 100 trials) on a data file
+def save_qtable(score, qtable):
+    file = open('qtable.data', 'wb')
+    pickle.dump((score, qtable), file)
+    file.close()
+
+# load qtable and his average score from the file (if it exist)
+def load_qtable():
+    try:
+        file = open('qtable.data')
+        current_score, qtable = pickle.load(file)
+    except:
+        print("Can't open the stored qtable. Maybe it's missing.")
+        return 0, 0
+        #exit(0)
+
+    return current_score, qtable
+
 # (1./(episode + 1)) this decrease the chance of choosing a greedly action based on the num_episode
 # we choose more greedly at the first ones
 # here we need to give a chance to choose other actions because, if not, we always will use the first action
@@ -55,7 +76,6 @@ def choose_action(state, episode):
         if random.randint(1, 10) > 8:
             action = 0
     return action
-
 
 # train our agent with n episodes
 def train_agent(num_episodes):
@@ -78,13 +98,15 @@ def train_agent(num_episodes):
 
 # test our agent to see how good is his bahavior in n epsodes with the current q-table
 # return the average score in n epsides
-def test_agent(num_tests, render):
+def test_agent(num_tests, render, slow_render):
     for i in range(num_tests):
         total_reward = 0
         state = env.reset()
         state = discrete_indexes(state)
         for j in range(200):
             if render == True:
+                if slow_render == True:
+                    time.sleep(0.2)
                 env.render()
             action = np.argmax(Q[state[0], state[1], state[2], :])
             new_state, reward, done, info = env.step(action)
@@ -101,7 +123,24 @@ def test_agent(num_tests, render):
 reward_list = []
 final_score = 0
 
-train_agent(num_train_episodes)
+if use_saved_qtable == True: # note that the test scores may difer
+    saved_score, Q = load_qtable()
+    print 'Using stored Qtable. Note that the test score may differ from the saved score (it will not be updated).'
+else:
+    saved_score, _ = load_qtable()
+    train_agent(num_train_episodes)
 
-final_score = test_agent(num_test_episodes, render)
-print(final_score)
+final_score = test_agent(num_test_episodes, render, slow_render)
+
+#print 'Score of the current Qtable in this test: ', final_score
+print 'Score of stored Qtable: ', saved_score
+
+# if this trial gets a better score than the saved one it save the new qtable
+if saved_score < final_score and use_saved_qtable == False:
+    save_qtable(final_score, Q)
+    print 'Saving current Qtable...'
+else:
+    # new score will never be saved
+    print("Current Qtable will not be saved.")
+
+print 'Final score: ', final_score
